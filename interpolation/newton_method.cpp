2f4dbb9239c834d2943d97dab0488eed245854bb
Diego Charrez <dcharrezt@unsa.edu.pe>
2018-06-20 10:35:09 -0500

    renamed files

---
+++ interpolation/newton_method.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef vector< pair<float, float> > coords;
+
+float function( float x ) {
+	return 1 / x; 
+}
+
+coords read_input() {
+	int n_dots;
+	float x_tmp;
+	coords dots;
+	
+	cin >> n_dots;
+	for(int i = 0; i < n_dots; i++) {
+		cin >> x_tmp;
+		dots.push_back( make_pair( x_tmp, function(x_tmp)) );
+	}
+	return dots;
+}
+
+void newton_method( coords dots, float p_x) {
+	float L = 0.; 
+ 	float L_tmp;
+	for (int i = 0; i < dots.size(); ++i) {
+		L_tmp = 1;
+		for (int j = 0; j < dots.size(); ++j) {
+			if( i != j )
+				L_tmp *= ((p_x - dots[j].first) / (dots[i].first - dots[j].first));
+		}
+		L_tmp *= dots[i].second;
+		L+=L_tmp;
+	}
+	cout << p_x << " " << L << endl;
+}
+
+int main() {
+
+	coords dots;
+	dots = read_input();
+
+	float p_x;
+	cin >> p_x;
+
+	for (int i = 0; i < dots.size(); ++i) {
+		cout << dots[i].first << ' ' << dots[i].second << endl;
+	}
+
+	// newton_method( dots, p_x);
+
+	return 0;
+}
