d475349bd485a8ca8e01db157abebc5cd70c8693
Diego Charrez <dcharrezt@unsa.edu.pe>
2018-06-20 09:52:11 -0500

    ploting lagrange interpolation

---
+++ interpolation/plot_dots.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+X = []
+Y = []
+
+
+with open("output.txt", 'r') as file:
+	for line in file:
+		asd= line.rstrip('\n').split()
+		print(asd)
+		X.append( float(asd[0]) )
+		Y.append( float(asd[1]) )
+
+plt.axes([0., 0., 1, 1])
+
+print(X)
+print(Y)
+
+plt.scatter(X, Y, s=10, c='black', alpha=.5)
+# plt.xlim(0, 5)
+# plt.xticks(())
+# plt.ylim(0, 1)
+# plt.yticks(())
+
+plt.rc('grid', linestyle="-", color='black')
+plt.grid(True)
+
+plt.show()
