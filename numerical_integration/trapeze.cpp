d266c110362a07731757f7fc9de2ca5991f9c8b6
Diego Charrez <dcharrezt@unsa.edu.pe>
2018-06-25 15:26:19 -0500

    simpson method for numerical integration

--- numerical_integration/trapeze.cpp
+++ numerical_integration/trapeze.cpp
@@ -6,17 +6,40 @@ float function( float x){
 	return x;
 }
 
-void trapeze_method(float a, float b, int n) {
+float function_2 ( float x ) {
+	return x * x;
+}
+
+void trapeze_method(float(*f)(float), float a, float b, int n) {
 	float h = ( b - a ) / (2.*n);
 	float sum = 0.;
 	float interval = float((b-a)/ n);
-	for (int i = 1; i < n-1; ++i) {
-		sum += function(  interval*i + a );
+	for (int i = 1; i <= n-1; ++i) {
+		sum += f(  interval*i + a );
 	}
 	sum *= 2;
-	sum += function(a) + function(b);
+	sum += f(a) + f(b);
 	sum *= (h);
-	cout << "Integral " << sum << endl;
+	cout << "Integral per Trapeze " << sum << endl;
+}
+
+void simpson_method( float(*f)(float), float a, float b, int n ) {
+	float h = (b-a)/(3.*n);
+	float sum = 0.;
+	float interval = float((b-a)/ n);
+	for (int i = 1; i <= (n/2)-1; ++i) {
+		sum += f( interval*i*2 + a);
+	}
+	sum *= 2;
+	float tmp = 0.;
+	for (int i = 1; i <= (n/2); ++i) {
+		tmp += f( interval*(2*i-1) + a );
+	}
+	tmp *= 4;
+	sum += f(a) + f(b) + tmp;
+	sum *= (h);
+	cout << "Integral per Simpson " << sum << endl;
+
 }
 
 int main() {
@@ -27,6 +50,7 @@ int main() {
 	cin >> a >> b;
 	cout << "Enter the number of partitions N " << endl;
 	cin >> n;
-	trapeze_method( a, b, n );
+	trapeze_method( function_2, a, b, n );
+	simpson_method( function_2, a, b, n );
 
 }
